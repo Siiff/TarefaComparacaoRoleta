06f128f9c9fd5a37b7b8c71e3ebe676134c5e61a
Siff <ricardodsgenari@gmail.com>
2021-03-04 10:13:56 -0300

    Release

---
+++ app/src/main/java/com/example/aula0103_tarefa/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.aula0103_tarefa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void RodaODado(View view){
+        TextView RodandoODado = findViewById(R.id.numeroArodar);
+        int x = new Random().nextInt(11);
+        RodandoODado.setText("Número: " + x);
+    }
+}
