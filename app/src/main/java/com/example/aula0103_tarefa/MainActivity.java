88d7f9121577a20937f787a38f8a92f6ee5aa9e7
Siff <ricardodsgenari@gmail.com>
2021-03-08 11:13:07 -0300

    Quase lá....

--- app/src/main/java/com/example/aula0103_tarefa/MainActivity.java
+++ app/src/main/java/com/example/aula0103_tarefa/MainActivity.java
@@ -7,29 +7,53 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textParabens;
+    private EditText aposta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aposta = findViewById(R.id.inputnumero);
+        textParabens = findViewById(R.id.caixaparabens);
     }
+
+
     public void RodaODado(View view){
         TextView RodandoODado = findViewById(R.id.numeroArodar);
         int randomnumber = new Random().nextInt(11);
         RodandoODado.setText("Número: " + randomnumber);
-    }
-    public void Parabens(View view){
-        TextView parabenizacao = findViewById(R.id.caixaparabens);
-        parabenizacao.setText("Parabens, Você gannhou!");
-    }
 
-    public void Comparacao(View view){
-        EditText numero = (EditText) findViewById(R.id.inputnumero);
-        
-    }
+        String numeroAposta;
+        numeroAposta = aposta.getText().toString();
 
+        Boolean campoValidado = validarcampo(numeroAposta);
+        if(campoValidado){
+            Double doubleDaAposta = Double.parseDouble(numeroAposta);
+            if(doubleDaAposta == randomnumber){
+                textParabens.setText("Parabens, Você Ganhou");
+            }
+            else {
+                textParabens.setText("Você perdeu, que peninha");
+            }
+        }
+        else {
+            textParabens.setText("Faça sua Aposta primeiro!");
+        }
+
+    }
+    public Boolean validarcampo (String apostaJogador){
+        Boolean campoValidado = true;
+        if(apostaJogador == null || apostaJogador.equals("")){
+            campoValidado = false;
+        }
+        return  campoValidado;
+    }
 
 }
